e1fa0c40651168d572ffddf77a75425f28494a67
physphil <physphil@gmail.com>
2018-02-17 10:51:52 -0500

    Added more test for MainActivityViewModel

--- app/src/test/java/com/physphil/android/remindme/MainActivityViewModelTest.kt
+++ app/src/test/java/com/physphil/android/remindme/MainActivityViewModelTest.kt
@@ -1,6 +1,8 @@
 package com.physphil.android.remindme
 
 import android.arch.core.executor.testing.InstantTaskExecutorRule
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.Observer
 import com.physphil.android.remindme.data.ReminderRepo
 import com.physphil.android.remindme.job.JobRequestScheduler
 import com.physphil.android.remindme.room.entities.Reminder
@@ -8,6 +10,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
 import org.mockito.MockitoAnnotations.initMocks
 
@@ -30,6 +33,9 @@ class MainActivityViewModelTest {
     @Mock
     private lateinit var scheduler: JobRequestScheduler
 
+    @Mock
+    private lateinit var observer: Observer<Any>
+
     private lateinit var viewModel: MainActivityViewModel
 
     @Before
@@ -39,18 +45,64 @@ class MainActivityViewModelTest {
     }
 
     @Test
+    fun testEmptyReminderListUpdated() {
+        val reminderList = MutableLiveData<List<Reminder>>()
+        reminderList.value = listOf()
+        `when`(repo.getActiveReminders()).thenReturn(reminderList)
+
+        viewModel = MainActivityViewModel(repo, scheduler)
+        viewModel.reminderListUpdated()
+        assert(viewModel.getSpinnerVisibility().value == false)
+        assert(viewModel.getEmptyVisibility().value == true)
+    }
+
+    @Test
+    fun testReminderListUpdated() {
+        val reminderList = MutableLiveData<List<Reminder>>()
+        reminderList.value = listOf(Reminder(), Reminder())
+        `when`(repo.getActiveReminders()).thenReturn(reminderList)
+
+        viewModel = MainActivityViewModel(repo, scheduler)
+        viewModel.reminderListUpdated()
+        assert(viewModel.getSpinnerVisibility().value == false)
+        assert(viewModel.getEmptyVisibility().value == false)
+    }
+
+    @Test
+    fun testConfirmDeleteAllReminders() {
+        viewModel.showDeleteAllConfirmationEvent.observeForever(observer as Observer<Void>)
+
+        viewModel.confirmDeleteAllReminders()
+        verify(observer).onChanged(null)
+    }
+
+    @Test
     fun testDeleteAllReminders() {
+        viewModel.clearNotificationEvent.observeForever(observer as Observer<Int?>)
+
         viewModel.deleteAllReminders()
         verify(scheduler).cancelAllJobs()
         verify(repo).deleteAllReminders()
+        verify(observer).onChanged(null)
+    }
+
+    @Test
+    fun testConfirmDeleteReminder() {
+        viewModel.showDeleteConfirmationEvent.observeForever(observer as Observer<Void>)
+
+        viewModel.confirmDeleteReminder(Reminder())
+        verify(observer).onChanged(null)
     }
 
     @Test
     fun testDeleteReminder() {
+        viewModel.clearNotificationEvent.observeForever(observer as Observer<Int?>)
         val reminder = Reminder()
+
         viewModel.confirmDeleteReminder(reminder)
         viewModel.deleteReminder()
         verify(scheduler).cancelJob(reminder.externalId)
         verify(repo).deleteReminder(reminder)
+        verify(observer).onChanged(reminder.notificationId)
     }
 }
