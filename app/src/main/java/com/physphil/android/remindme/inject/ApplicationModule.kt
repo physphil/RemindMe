2c1f34a64aa0684bffdea90bf9078886659c4384
Phil Shadlyn <phil.shadlyn@shopify.com>
2018-03-24 14:22:41 -0400

    Inject AppDatabase directly into ReminderRepo

--- app/src/main/java/com/physphil/android/remindme/inject/ApplicationModule.kt
+++ app/src/main/java/com/physphil/android/remindme/inject/ApplicationModule.kt
@@ -1,6 +1,8 @@
 package com.physphil.android.remindme.inject
 
+import android.arch.persistence.room.Room
 import android.content.Context
+import com.physphil.android.remindme.DATABASE_NAME
 import com.physphil.android.remindme.MainActivityViewModelFactory
 import com.physphil.android.remindme.RemindMeApplication
 import com.physphil.android.remindme.data.ReminderRepo
@@ -25,7 +27,9 @@ class ApplicationModule(private val application: RemindMeApplication) {
 
     @Provides
     @Singleton
-    fun providesReminderRepo(): ReminderRepo = ReminderRepo(AppDatabase.getInstance(application).reminderDao())
+    fun providesReminderRepo(): ReminderRepo = ReminderRepo(Room.databaseBuilder(application.applicationContext, AppDatabase::class.java, DATABASE_NAME)
+            .build()
+            .reminderDao())
 
     @Provides
     fun providesMainActivityViewModelFactory(repo: ReminderRepo, scheduler: JobRequestScheduler) = MainActivityViewModelFactory(repo, scheduler)
