2540e4b2d17f378963e88ab6d169f47d85bd25a9
Phil Shadlyn <phil.shadlyn@shopify.com>
2018-11-25 17:28:07 -0500

    GH-8 Removed Butterknife from ProgressSpinner

--- app/src/main/java/com/physphil/android/remindme/ui/ProgressSpinner.kt
+++ app/src/main/java/com/physphil/android/remindme/ui/ProgressSpinner.kt
@@ -1,24 +1,23 @@
 package com.physphil.android.remindme.ui
 
 import android.content.Context
+import android.util.AttributeSet
 import androidx.annotation.StringRes
 import androidx.constraintlayout.widget.ConstraintLayout
-import android.util.AttributeSet
-import android.widget.ProgressBar
-import android.widget.TextView
-import butterknife.BindView
-import butterknife.ButterKnife
 import com.physphil.android.remindme.R
+import kotlinx.android.synthetic.main.view_progress_spinner.view.*
 
 /**
  * Copyright (c) 2018 Phil Shadlyn
  */
-class ProgressSpinner : ConstraintLayout {
-    constructor(context: Context) : this(context, null)
-    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
-    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-        val view = inflate(context, R.layout.view_progress_spinner, this)
-        ButterKnife.bind(this, view)
+class ProgressSpinner @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.view_progress_spinner, this)
 
         attrs?.let {
             // Load any xml-specified attributes
@@ -28,25 +27,18 @@ class ProgressSpinner : ConstraintLayout {
 
             if (res != 0) {
                 setMessage(res)
-            }
-            else if (str != null) {
+            } else if (str != null) {
                 setMessage(str)
             }
             ta.recycle()
         }
     }
 
-    @BindView(R.id.progress_spinner)
-    lateinit var spinner: ProgressBar
-
-    @BindView(R.id.progress_message)
-    lateinit var message: TextView
-
     fun setMessage(@StringRes message: Int) {
-        this.message.setText(message)
+        progressMessageView.setText(message)
     }
 
     fun setMessage(message: String) {
-        this.message.text = message
+        progressMessageView.text = message
     }
 }
