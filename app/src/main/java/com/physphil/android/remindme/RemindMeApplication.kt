8324cb4e4acdae8548a55fd3dd0baa04f458c649
Phil Shadlyn <phil.shadlyn@shopify.com>
2019-08-22 14:33:23 -0400

    GH-20: remove Dagger and components

--- app/src/main/java/com/physphil/android/remindme/RemindMeApplication.kt
+++ app/src/main/java/com/physphil/android/remindme/RemindMeApplication.kt
@@ -2,9 +2,6 @@ package com.physphil.android.remindme
 
 import android.app.Application
 import com.evernote.android.job.JobManager
-import com.physphil.android.remindme.inject.ApplicationComponent
-import com.physphil.android.remindme.inject.ApplicationModule
-import com.physphil.android.remindme.inject.DaggerApplicationComponent
 import com.physphil.android.remindme.inject.Injector
 import com.physphil.android.remindme.job.GlobalJobCreator
 
@@ -13,24 +10,13 @@ import com.physphil.android.remindme.job.GlobalJobCreator
  */
 open class RemindMeApplication : Application() {
 
-    val applicationComponent: ApplicationComponent by lazy {
-        DaggerApplicationComponent.builder()
-            .applicationModule(ApplicationModule(this))
-            .build()
-    }
-
     override fun onCreate() {
         super.onCreate()
-        instance = this
         JobManager.create(this).addJobCreator(
             GlobalJobCreator(
                 repo = Injector.provideReminderRepo(this),
                 scheduler = Injector.provideJobRequestScheduler()
             )
         )
-    }
-
-    companion object {
-        lateinit var instance: RemindMeApplication
     }
 }
