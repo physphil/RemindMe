98528321b90fbddb2364437095df31877295a961
Phil Shadlyn <phil.shadlyn@shopify.com>
2020-10-31 00:05:15 -0400

    GH-78: remove 'Delete' option from Reminder Details page

--- app/src/main/java/com/physphil/android/remindme/reminders/ReminderActivity.kt
+++ app/src/main/java/com/physphil/android/remindme/reminders/ReminderActivity.kt
@@ -98,22 +98,11 @@ class ReminderActivity : BaseActivity(),
         return true
     }
 
-    override fun onPrepareOptionsMenu(menu: Menu): Boolean {
-        super.onPrepareOptionsMenu(menu)
-        val delete = menu.findItem(R.id.menu_delete)
-        delete?.let { viewModel.prepareOptionsMenuItems(delete) }
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_save -> {
                 viewModel.saveReminder()
                 finish()
-                true
-            }
-            R.id.menu_delete -> {
-                viewModel.confirmDeleteReminder()
                 true
             }
             else -> super.onOptionsItemSelected(item)
